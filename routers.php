8e58b7a46d08fc0ddf6662d5e4f8a31c293415ac
esvlad <svladpro@yandex.ru>
2024-08-21 13:58:55 +0500

    routers init

---
+++ routers.php
@@ -0,0 +1,22 @@
+<?php
+
+use MiladRahimi\PhpRouter\Router;
+use Laminas\Diactoros\Response\JsonResponse;
+
+use App\Controllers\{
+    Leads,
+};
+
+$router = Router::create();
+
+$router->get('/', function() {
+    return 'Home page!';
+});
+
+
+$router->group(['prefix' => '/lead'], function (Router $router) {
+    $router->get('/add/{cloud_id}', [Leads::class, 'add']);
+    $router->get('/update/{cloud_id}', [Leads::class, 'update']);
+});
+
+$router->dispatch();
